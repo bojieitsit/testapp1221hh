394aad48191e6489c62f2504b06defb50b08f5e5
Andrei <bojieitsit@icloud.com>
2023-08-10 13:38:51 +0400

    Initial Commit

---
+++ testapp1221hh/testapp1221hhApp.swift
@@ -0,0 +1,17 @@
+//
+//  testapp1221hhApp.swift
+//  testapp1221hh
+//
+//  Created by Andrei Bogoslovskii on 10.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct testapp1221hhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
