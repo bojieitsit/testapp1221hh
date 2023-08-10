f0de70a64eec994209886993399b61521b4e807a
Andrei <bojieitsit@icloud.com>
2023-08-10 23:17:25 +0400

    almost done

--- testapp1221hh/ContentView.swift
+++ testapp1221hh/ContentView.swift
@@ -12,7 +12,71 @@ struct ContentView: View {
     var body: some View {
         
         TabView {
-            VStack{
+            ScrollView{
+                
+                ScrollView(.horizontal){
+                    HStack(spacing: 10) {
+                        VStack{
+                            Image("1")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .cornerRadius(20.0)
+                            Text("Привелегии SPAR")
+                                .font(.system(size: 13.0))
+                                .multilineTextAlignment(.center)
+                                .frame(width: 80)
+                        }
+                        VStack{
+                            Image("2")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .cornerRadius(20.0)
+                            Text("Мы в соцсетях")
+                                .font(.system(size: 13.0))
+                                .multilineTextAlignment(.center)
+                                .frame(width: 80)
+                        }
+                        VStack{
+                            Image("3")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .cornerRadius(20.0)
+                            Text("3 рецепта коктейлей")
+                                .font(.system(size: 13.0))
+                                .multilineTextAlignment(.center)
+                                .frame(width: 80)
+                        }
+                        VStack{
+                            Image("4")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .cornerRadius(20.0)
+                            Text("Дегустации в SPAR")
+                                .font(.system(size: 13.0))
+                                .multilineTextAlignment(.center)
+                                .frame(width: 80)
+                        }
+                    }
+                    .frame(height: 110.0)
+                }
+                .scrollIndicators(.hidden)
+                .padding()
+                
+                ScrollView(.horizontal){
+                    HStack {
+                        Image("sale")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(20.0)
+                        Image("sale")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(20.0)
+                    }
+                    .frame(height: 130.0)
+                }
+                .scrollIndicators(.hidden)
+                .padding()
                 
                 Image("bonuses")
                     .resizable()
@@ -27,7 +91,11 @@ struct ContentView: View {
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .cornerRadius(20.0)
-                        Image("coffee")
+                        Image("cofesale")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(20.0)
+                        Image("cardsale")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .cornerRadius(20.0)
@@ -35,11 +103,11 @@ struct ContentView: View {
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .cornerRadius(20.0)
-                        Image("coffee")
+                        Image("cofesale")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .cornerRadius(20.0)
-                        Image("coffee")
+                        Image("cardsale")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .cornerRadius(20.0)
@@ -49,33 +117,42 @@ struct ContentView: View {
                 .scrollIndicators(.hidden)
                 .padding()
                 
-                ScrollView(.horizontal){
-                    HStack {
-                        Image("coffee")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .cornerRadius(20.0)
-                        Image("coffee")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .cornerRadius(20.0)
-                        Image("coffee")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .cornerRadius(20.0)
-                        Image("coffee")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .cornerRadius(20.0)
-                        Image("coffee")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .cornerRadius(20.0)
+                VStack(){
+                    Text("Рекомендуем")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                       
+                    ScrollView(.horizontal){
+                        HStack {
+                            Image("coffee")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .cornerRadius(20.0)
+                            Image("coffee")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .cornerRadius(20.0)
+                            Image("coffee")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .cornerRadius(20.0)
+                            Image("coffee")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .cornerRadius(20.0)
+                            Image("coffee")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .cornerRadius(20.0)
+                        }
+                        .frame(height: 130.0)
                     }
-                    .frame(height: 130.0)
+                    .scrollIndicators(.hidden)
+                    
                 }
-                .scrollIndicators(.hidden)
                 .padding()
+                
                     
             }
             .tabItem {
